db9cc87bb2044ac724c1b8ecdce8437627acd87d
villalpandoc <villalpandoc@gmail.com>
2014-03-23 16:46:39 -0500

    uesless spritetest.js

--- CastleClashersClient/js/spritetest.js
+++ CastleClashersClient/js/spritetest.js
@@ -1,9 +1,10 @@
 window.onload = function() {
 	enchant();
-	var game = new Game(1, 1); game.preload('swords.png');
+	var game = new Core(320, 320); 
+	game.preload("assets/swords.png");
 	game.onload = function() {
 		var sword = new Sprite(21, 21);
-		sword.image = game.assets['/assets/swords.png'];
+		sword.image = game.assets["assets/swords.png"];
 		game.rootScene.addChild(sword);
 	};
 	game.start();
